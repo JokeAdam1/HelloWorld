6d612d832eb9874aa5e83baf43b0a0b5761286a3
Joke Adam <joke.adam@student.vives.be>
2018-09-20 13:39:08 +0200

    versie 1

--- src/main/java/be/vives/ti/Show.java
+++ src/main/java/be/vives/ti/Show.java
@@ -1,4 +1,19 @@
 package be.vives.ti;
 
 public class Show {
+    private String tekst;
+
+    public Show(String tekst) {
+        this.tekst = tekst;
+    }
+
+    public void setTekst(String nieuwetekst)
+    {
+        tekst = nieuwetekst;
+    }
+
+    public String getTekst()
+    {
+        return tekst;
+    }
 }
